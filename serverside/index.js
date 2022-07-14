33b041bd51d23408c74a77ffc2e760f6c01f2d0e
prabhakar <prabhakarsrivastava73@gmial.com>
2022-07-14 19:28:22 +0530

    a-done

--- serverside/index.js
+++ serverside/index.js
@@ -1,45 +1,122 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
+const cors = require('cors');
+const bodyparser = require("body-parser");
+const bcrypt = require('bcrypt');
+const saltround = 10;
 
-const db =mysql.createConnection({
-    host:"localhost",
+
+const db = mysql.createConnection({
+    host: "localhost",
     user: "root",
-    password:"",
-    database:"register"
+    password: "",
+    database: "register"
 });
-db.connect(err=> {
-    // if(err) throw err;
-    if(err){
-        console.log("reeor")
+db.connect(err => {
+    // if(err) throw err
+    if (!err) {
+        console.log("+ve")
+
     }
 });
 
-app.get('/',(req,res) =>{
-    
-    db.query(
-        'INSERT INTO reg (name , date , email , password) VALUES ("QWERTY1","2022-02-22","QWERTY@qwerty","12345678");'
-        , (err, result) => {
-            if(err){
-                res.send(err);
+app.use(cors());
+app.use(express.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+//test fetching
+
+// app.get('/api/get', (req, res) => {
+//     const qwe = "SELECT * FROM reg;";
+//     console.log(qwe);
+//     db.query(qwe, (err, result) => {
+//         console.log(result);
+//         res.send(result);
+//     });
+// });
+
+app.post("/api/insert", (req, res) => {
+
+    const regName = req.body.regName;
+    console.log( regName);
+    const regDate = req.body.regDate;
+    console.log(typeof(regDate));
+    console.log(regDate);
+    const regEmail = req.body.regEmail;
+    console.log(typeof(regEmail));
+    console.log(regEmail);
+    const regPassword = req.body.regPassword;
+    console.log(typeof(regEmail));
+
+    if(regPassword===""){
+        res.send({message:'fill all inputss'})
+    }else{
+        bcrypt.hash(regPassword, saltround, (err, hash) => {
+            if (err) {
+                console.log(err)
             }
-            res.send(result);
+            const query = 'INSERT INTO reg (name , date , email , password) VALUES (?,?,?,?);';
+    
+            db.query(query, [regName, regDate, regEmail, hash],
+                (err, result) => {
+                    if (err) {
+                        consple.log(err);
+                    }
+                    console.log(result);
+                }
+    
+            );
+            res.send({message:"Successfully Register. please go back to login"})
+        });
 
-    });
-
-    // db.query(
-    //     'INSERT INTO contries (contryName, population) VALUES ("africa", 20)',
-    //     (err, result) => {
-    //         if (err) {
-    //             console.log(err);
-    //         }
-    //         res.send(result);
-    //     }
-    //     );
-   
+    }
+    // if(regDate==''){
+    //     res.send({message:'plaese enter name'})
+        
+    // }
+    
+    
 });
 
-app.listen(3005, ()=>{
+app.post("/api/Login", (req, res) => {
+    const username = req.body.username;
+    console.log(username);
+    const password = req.body.password;
+    console.log(password);
+    if(username===''){
+        res.send({message:"fill all inputs"});
+    }else{
+
+        db.query(
+            "SELECT * FROM reg WHERE name = ?;",
+            username,
+            (err, result) => {
+                if (err) {
+                    res.send({ err: err });
+                }
+                
+                //checkin user 
+                if (result.length > 0) {
+                    bcrypt.compare(password, result[0].password, (error, response) => {
+                        if (response) {
+                            res.send(result);
+                            console.log(result);
+                        } else {
+                            res.send({ message: "Wrong username/password combination!" });
+                        }
+                    });
+                } else {
+                    res.send({ message: "user doesn't exist" });
+                }
+    
+    
+        });
+    }
+});
+
+
+app.listen(3001, () => {
     console.log("server sunning");
 
 });
